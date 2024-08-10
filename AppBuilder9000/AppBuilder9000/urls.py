10148285d10bc89074fb7c0e58024dc6d0f4966f
Miguel-stacb <64321960+Miguel-stacb@users.noreply.github.com>
2024-08-10 18:34:59 -0500

    Respaldo del proyecto en punto funcional

--- AppBuilder9000/AppBuilder9000/urls.py
+++ AppBuilder9000/AppBuilder9000/urls.py
@@ -20,6 +20,7 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
+    path('', views.index, name='index'),
+    path('MovieReviews/', include('MovieReviews.urls')),
 
 ]
